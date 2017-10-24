c65c0ddcb9ca28677aff1dd29a6512460c03bcaf
zouyingjie <yingjiezou1@gmail.com>
2017-10-24 23:23:57 +0800

    修改脚本

--- init_python_env.py
+++ init_python_env.py
@@ -5,15 +5,16 @@ import os
 login_name = "root"
 user_name = "zouyingjie"
 
-# os.mkdir("/home/zouyingjie/source_code")
-# os.system("cd /home/zouyingjie/source_code")
-# os.system("wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tgz")
-# os.system("tar -zxvf Python-3.6.2.tgz")
-# os.system("cd /home/zouyingjie/source_code/Python-3.6.2")
-# os.system("./configure --prefix=/usr/local/ --enable-shared")
-os.system("cd /home/zouyingjie/source_code/EnvScript/Python-3.6.2")
-os.system("make and make install")
-
+os.system("useradd zouyingjie")
+os.system("mkdir /home/zouyingjie/source_code")
+os.system("cd /home/zouyingjie/source_code")
+os.system("pwd")
+os.system("wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tgz")
+os.system("tar -zxvf Python-3.6.2.tgz")
+os.system("cd /home/zouyingjie/source_code/Python-3.6.2")
+os.system("./configure --prefix=/usr/local/ --enable-shared")
+os.system("make")
+os.system("make install")
 os.system('echo "export PATH=/usr/local/python/bin:$PATH" &>> /root/.bashrc')
 os.system('echo "export PATH=/usr/local/python/bin:$PATH" &>> /home/zouyingjie/.bashrc')
 
