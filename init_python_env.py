0277c6a6333afb0d10597037238f2f948b2a74b2
zouyingjie <yingjiezou1@gmail.com>
2017-10-24 21:15:13 +0800

    修改脚本

--- init_python_env.py
+++ init_python_env.py
@@ -5,11 +5,11 @@ import os
 login_name = "root"
 user_name = "zouyingjie"
 
-os.mkdir("/home/zouyingjie/source_code")
-os.system("cd /home/zouyingjie/source_code")
-os.system("wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tgz")
-os.system("tar -zxvf Python-3.6.2.tgz")
-os.system("cd Python-3.6.2")
+# os.mkdir("/home/zouyingjie/source_code")
+# os.system("cd /home/zouyingjie/source_code")
+# os.system("wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tgz")
+# os.system("tar -zxvf Python-3.6.2.tgz")
+os.system("cd /home/zouyingjie/source/code/Python-3.6.2")
 os.system("./configure --prefix=/usr/local/ --enable-shared")
 os.system("make and make install")
 
