d62edcb7c9767c5cf0faecd6a5d63ecedf4698d8
zouyingjie <yingjiezou1@gmail.com>
2017-10-24 21:17:15 +0800

    修改脚本

--- init_python_env.py
+++ init_python_env.py
@@ -9,7 +9,7 @@ user_name = "zouyingjie"
 # os.system("cd /home/zouyingjie/source_code")
 # os.system("wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tgz")
 # os.system("tar -zxvf Python-3.6.2.tgz")
-os.system("cd /home/zouyingjie/source/code/Python-3.6.2")
+os.system("cd /home/zouyingjie/source_code/Python-3.6.2")
 os.system("./configure --prefix=/usr/local/ --enable-shared")
 os.system("make and make install")
 
